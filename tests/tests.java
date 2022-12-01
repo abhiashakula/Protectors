25b9f5c710096e91aeca0937a2115b429f3a2953
navyareddy <nnallala@slu.edu>
2022-12-01 15:34:50 -0600

    order odf test cases

--- tests/tests.java
+++ tests/tests.java
@@ -2,7 +2,11 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Path;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
@@ -10,53 +14,47 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
+//import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.runners.MethodSorters;
 import org.junit.jupiter.api.Order;
 
 import atm_fingerprint.ATMMain;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class tests {
-
-    private static ATMMain ui;
-
-    //setting the variables before the execution of test cases
-    @BeforeClass
+	
+	private static ATMMain ui;
+	
+	@BeforeClass
     public static void setup() {
-        try {
-            ui = new ATMMain();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+		try {
+			ui = new ATMMain();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}       
     }
-
-    //clearing the DB after execution of all test cases as we are creating dummy user
-    @AfterClass
+	
+	@AfterClass
     public static void clearDB() {
-        if(ui != null) {
-            String UserQuery = "DELETE FROM `users` WHERE `name` = 'NewTestUser' ";
+		if(ui != null) {
+        	String uQuerry = "DELETE FROM `users` WHERE `name` = 'NewTestUser' ";
             PreparedStatement query;
-            try {
-                query = (PreparedStatement) ui.conn.prepareStatement(UserQuery);
-                query.execute();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
+			try {
+				query = (PreparedStatement) ui.conn.prepareStatement(uQuerry);
+				query.execute();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 
-            ui.dispose();
+        	ui.dispose();
         }
-    }
-
-    //Test case to check if the UI is visible after running the code.
+   }
+	
     @Test
-    public void UiOpen() {
-        ui.setVisible(true);
-        assertTrue(ui.isVisible());
-    }
-
-
+    public void test1_UiOpen() {
+    	ui.setVisible(true);
+		assertTrue(ui.isVisible());		
+	}
 }
